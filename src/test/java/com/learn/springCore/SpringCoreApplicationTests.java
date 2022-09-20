fec94c8532b4b3ba89311a8c7f86c300e0490058
Rafael_Silva <rafael.ds81@gmail.com>
2022-09-20 23:53:51 +0100

    Initial commit

---
+++ src/test/java/com/learn/springCore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
