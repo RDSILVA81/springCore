ccd0016af27efec0de7da2fe893baec746a584f4
Rafael_Silva <rafael.ds81@gmail.com>
2022-10-25 23:45:14 +0100

    format

---
+++ src/main/java/com/learn/springCore/flyweightPattern/TestFormat.java
@@ -0,0 +1,7 @@
+package com.learn.springCore.flyweightPattern;
+
+public class TestFormat {
+    public static void main(String[] args) {
+        System.out.println(String.format("%08d/%03d", 1,2));
+    }
+}
